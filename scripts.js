7c79eb4a548a84fd66203bc7c2c4772870e3c7d4
yep2035 <barjonasimon@icloud.com>
2024-10-08 23:31:06 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,22 @@
+// Countdown Timer
+const countdownDate = new Date("October 31, 2024 00:00:00").getTime();
+
+const countdownFunction = setInterval(() => {
+    const now = new Date().getTime();
+    const distance = countdownDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerText = days;
+    document.getElementById("hours").innerText = hours;
+    document.getElementById("minutes").innerText = minutes;
+    document.getElementById("seconds").innerText = seconds;
+
+    if (distance < 0) {
+        clearInterval(countdownFunction);
+        document.getElementById("timer").innerHTML = "Campaign Started!";
+    }
+}, 1000);
